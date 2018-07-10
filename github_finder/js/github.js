f3d6391c1d6218c151aa728b4bd10ea53e3c0408
aruzikulov <ruzikulov@gmail.com>
2018-07-11 01:33:16 +0800

    ok

---
+++ github_finder/js/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '3be81cab86b2db692489';
+    this.client_secret = 'a7a9bb268f6801a0095d4089b42174ed3a09513d';
+    this.repos_count = 10;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
